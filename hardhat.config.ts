a02aac9c79e91751138d00cb764aff9562e8e636
Willian Goncalves Rios <williangrios@yahoo.com.br>
2023-07-29 23:16:23 -0300

    add etherscan api key config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,15 +12,18 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         url: "https://eth-mainnet.g.alchemy.com/v2/SKEZZEasoBhw2PixrKv9m2r8ZvB1mmnp",
-        blockNumber: 17801589
-      }
+        blockNumber: 17801589,
+      },
     },
     //setting mumbai testnet
     // mumbai: {
     //   url: process.env.MUMBAI_RPC_URL as string,
     //   accounts: [process.env.MAIN_ACCOUNT as string]
     // }
-  }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
